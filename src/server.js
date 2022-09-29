724233ff4398e3ca7fabdd5d4500bff086f559ba
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-09-29 15:18:31 -0300

    feat: games get and post implementation

--- src/server.js
+++ src/server.js
@@ -48,6 +48,37 @@ app.get('/categories', async (req, res) => {
   }
 });
 
+app.post('/games', async (req, res) => {
+  const game = req.body;
+
+  try {
+    const isValid = (await connection.query('SELECT id FROM games WHERE name = $1', [game.name])).rowCount;
+
+    //fazer validação joi para não ser vazio
+    if (isValid === 0) {
+      await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5);', [game.name, game.image, game.stockTotal, game.categoryId, game.pricePerDay]);
+      return res.sendStatus(201);
+    }
+
+    res.sendStatus(409);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.get('/games', async (req, res) => {
+  try {
+    const games = (await connection.query('SELECT * FROM games;')).rows;
+
+    res.send(games);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listen on port ${process.env.PORT}`);
 });
