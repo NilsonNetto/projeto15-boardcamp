94bfc24f488eeb0b0cc0c35368fea3b01a9c5d9f
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-10-01 15:24:23 -0300

    feat: customers routes final implementation

--- controllers/customers.controllers.js
+++ controllers/customers.controllers.js
@@ -1,16 +1,28 @@
 import connection from "../db/db.js";
+import { customerSchema } from "../schemas/customers.schema.js";
 
 const createCustomer = async (req, res) => {
   const { name, phone, cpf, birthday } = req.body;
 
-  //fazer validação joi
+  const validation = customerSchema.validate({
+    name,
+    phone,
+    cpf,
+    birthday,
+  }, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map(error => error.message);
+    return res.status(400).send(errors);
+  }
+
   try {
 
-    const isValid = (await connection.query('SELECT id FROM customers WHERE cpf = $1;', [cpf])).rowCount;
+    const isRepeated = (await connection.query('SELECT id FROM customers WHERE cpf = $1;', [cpf])).rowCount;
 
-    if (isValid === 0) {
-
+    if (!isRepeated) {
       await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4);', [name, phone, cpf, birthday]);
+
       return res.sendStatus(201);
     }
     res.sendStatus(409);
@@ -24,15 +36,32 @@ const updateCustomer = async (req, res) => {
   const userId = req.params.id;
   const { name, phone, cpf, birthday } = req.body;
 
-  //fazer validação joi
+  const validation = customerSchema.validate({
+    name,
+    phone,
+    cpf,
+    birthday,
+  }, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map(error => error.message);
+    return res.status(400).send(errors);
+  }
+
   try {
 
     const customer = (await connection.query('SELECT * FROM customers WHERE id = $1;', [userId])).rows[0];
 
     if (customer) {
 
-      await connection.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;', [name, phone, cpf, birthday, userId]);
-      return res.sendStatus(201);
+      const isRepeated = await connection.query('SELECT cpf FROM customers WHERE cpf = $1;', [cpf]);
+
+      if (!isRepeated.rowCount || customer.cpf === isRepeated.rows[0].cpf) {
+        await connection.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;', [name, phone, cpf, birthday, userId]);
+        return res.sendStatus(201);
+      }
+
+      return res.sendStatus(409);
     }
     res.sendStatus(404);
   } catch (error) {
@@ -49,9 +78,9 @@ const listCustomers = async (req, res) => {
       const customers = (await connection.query('SELECT * FROM customers WHERE cpf LIKE $1;', [`${cpf}%`])).rows;
       return res.send(customers);
     }
+
     const customers = (await connection.query('SELECT * FROM customers;')).rows;
     res.send(customers);
-
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
